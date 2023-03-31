bd5af7ca1ab69e120af97758faa998b5067768c4
HAMMAD <108028597+HAMMAD-DEVELOPER-TECH@users.noreply.github.com>
2023-03-31 11:49:26 +0500

    Event - KeyCode

---
+++ script.js
@@ -0,0 +1,16 @@
+const inser = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  inser.innerHTML = `<div class="key">
+    ${e.key===' '? 'space': e.key}
+    <small>event.key</small>
+    </div>
+    <div class="key">
+        ${e.keyCode}
+        <small>event.keyCode</small>
+    </div>
+    <div class="key">
+        ${e.code}
+        <small>event.Code</small>
+    </div>`;
+});
